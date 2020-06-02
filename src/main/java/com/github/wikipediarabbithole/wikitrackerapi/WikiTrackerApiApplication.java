fa17d689a218f715c4b0e6faddf1c94767b01310
Ian Roberts <irob466@gmail.com>
2020-06-02 07:53:26 -0500

    intertial kermit

---
+++ src/main/java/com/github/wikipediarabbithole/wikitrackerapi/WikiTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.wikipediarabbithole.wikitrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wikipediarabbithole/wikitrackerapi/WikiTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wikipediarabbithole.wikitrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
