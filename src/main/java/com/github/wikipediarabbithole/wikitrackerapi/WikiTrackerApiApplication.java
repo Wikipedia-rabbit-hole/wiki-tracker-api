b7d386a15053c71b0953be572a871693790edce0
Ian Roberts <irob466@gmail.com>
2020-06-02 22:09:46 -0500

    added in graphql config

--- src/main/java/com/github/wikipediarabbithole/wikitrackerapi/WikiTrackerApiApplication.java
+++ src/main/java/com/github/wikipediarabbithole/wikitrackerapi/WikiTrackerApiApplication.java
@@ -1,13 +1,30 @@
 package com.github.wikipediarabbithole.wikitrackerapi;
 
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static graphql.Scalars.GraphQLString;
 
 @SpringBootApplication
 public class WikiTrackerApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WikiTrackerApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WikiTrackerApiApplication.class, args);
+    }
 
+    @Bean
+    GraphQLSchema schema() {
+        return GraphQLSchema.newSchema()
+                .query(GraphQLObjectType.newObject()
+                        .name("query")
+                        .field(field -> field
+                                .name("test")
+                                .type(GraphQLString)
+                                .dataFetcher(environment -> "response"))
+                        .build())
+                .build();
+    }
 }
